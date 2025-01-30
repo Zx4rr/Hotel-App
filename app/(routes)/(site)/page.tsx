7d7b3bc5184230424b12846fbc24bc124c97d18f
Zx4rr <cinarsenturk.3456@gmail.com>
2025-01-30 08:48:43 +0300

    30.01.25 tarihli düzenleme

--- app/(routes)/(site)/page.tsx
+++ app/(routes)/(site)/page.tsx
@@ -1,12 +1,16 @@
 import { ModeToggle } from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
-
+import ImageVawes from "@/components/ImageWaves";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-slate-600">
       <Button variant="destructive" size="icon">ABC</Button>
+
       <ModeToggle />
+
+      <ImageWaves />
     </div>
   );
 }
